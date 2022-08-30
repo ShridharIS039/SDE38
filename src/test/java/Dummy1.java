9b01770a5a8fa8182444ba7f88d9973152fcda1e
Shridhar s <Shridharsirpee15@gmail.com>
2022-08-30 10:25:03 +0530

    new down

---
+++ src/test/java/Dummy1.java
@@ -0,0 +1,13 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Dummy1 {
+	
+	@Test
+	public void validTest() {
+		
+		Reporter.log("valid test is passed");
+		
+	}
+
+}
